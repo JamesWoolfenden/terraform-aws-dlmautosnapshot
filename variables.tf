fc7ac0b57880026144303909aad6e752c708a1b0
James Woolfenden <james.woolfenden@gmail.com>
2019-01-22 23:42:59 +0000

    fmt

--- variables.tf
+++ variables.tf
@@ -1,3 +1,10 @@
 variable "schedule" {
   type = "list"
 }
+
+# this is enables you to add dependancies
+variable depends_on {
+  default     = []
+  description = "This is a way to make a module depends on, which isnt built in."
+  type        = "list"
+}
