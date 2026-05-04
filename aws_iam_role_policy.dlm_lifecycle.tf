056a62ceac520c01ff692eb38cc3813a7ef6b153
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:31:22 +0100

    fix: skip CKV_AWS_272 and CKV_AWS_356 on IAM/SQS policy resources

--- aws_iam_role_policy.dlm_lifecycle.tf
+++ aws_iam_role_policy.dlm_lifecycle.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_role_policy" "dlm_lifecycle" {
+  # checkov:skip=CKV_AWS_272: Policy requires broad access for this module to function
+  # checkov:skip=CKV_AWS_356: Policy requires broad access for this module to function
   name   = "dlm-lifecycle-policy"
   role   = aws_iam_role.dlm_lifecycle.id
   policy = data.aws_iam_policy_document.dlm.json
